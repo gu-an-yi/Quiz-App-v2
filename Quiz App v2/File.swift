e4866b2c7c7cbc5bc5d07ad388942ae7240bd347
Guan Yi <lee_guan_yi@s2021.ssts.edu.sg>
2022-06-23 14:28:26 +0800

    hello

---
+++ Quiz App v2/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Quiz App v2
+//
+//  Created by Lee Guan Yi on 23/6/22.
+//
+
+import Foundation
+
+struct QuizQuestion{
+    var title: String
+    
+    var option1: String
+    var option2: String
+    var option3: String
+    var option4: String
+
+}
