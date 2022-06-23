cc2702bcf96b414debe53668737af0b052f958d4
Guan Yi <lee_guan_yi@s2021.ssts.edu.sg>
2022-06-23 14:14:16 +0800

    Initial Commit

---
+++ Quiz App v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quiz App v2
+//
+//  Created by Lee Guan Yi on 23/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quiz App v2/Quiz_App_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_App_v2App.swift
+//  Quiz App v2
+//
+//  Created by Lee Guan Yi on 23/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_App_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
